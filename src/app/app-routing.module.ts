92b0a0aaf30df2e002df5ba861b76edbddd2b4b5
DannyFrayre <dannyfrayresanchez1324@outlook.com>
2019-03-26 14:50:52 -0600

    inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import{MujeresComponent} from'./components/mujeres/mujeres.component';
+import {TendenciasComponent } from './components/tendencias/tendencias.component';
+import{RegistrarseComponent} from'./components/registrarse/registrarse.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path:'', component: InicioComponent},
+  {path:'mujeres', component: MujeresComponent},
+  {path:'registrarse',component:RegistrarseComponent},
+  {path:'tendencias', component:TendenciasComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlumnosComponent } from './components/alumnos/alumnos.component';
+import { AlumnosListComponent } from './components/alumnos-list/alumnos-list.component';
+import { AlumnoComponent } from './components/alumnos/alumno/alumno.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { AmigosComponent } from './components/amigos/amigos.component';
+import { RegistrarComponent } from './components/registrar/registrar.component';
+import { MujeresComponent } from './components/mujeres/mujeres.component';
+import { TendenciasComponent } from './components/tendencias/tendencias.component';
+import { RegistrarseComponent } from './components/registrarse/registrarse.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlumnosComponent,
+    AlumnosListComponent,
+    AlumnoComponent,
+    InicioComponent,
+    ContactoComponent,
+    AmigosComponent,
+    RegistrarComponent,
+    MujeresComponent,
+    TendenciasComponent,
+    RegistrarseComponent
   ],
   imports: [
     BrowserModule,
